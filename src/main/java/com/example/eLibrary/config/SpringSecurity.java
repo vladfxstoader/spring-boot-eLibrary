504d309486f099637a1d74ec99e028537d440455
Vlad Toader <toadervlad01@gmail.com>
2024-05-07 18:18:23 +0300

    started working on h2 database

--- src/main/java/com/example/eLibrary/config/SpringSecurity.java
+++ src/main/java/com/example/eLibrary/config/SpringSecurity.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
@@ -28,8 +30,12 @@ public class SpringSecurity {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                        .disable())
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/register/**", "/", "/forgot-password/**", "/access_denied","/favicon2.png").permitAll()
+                             .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/authors/**", "/add-author/**", "/add-publisher/**", "/publishers",
                                 "/categories", "/add-category/**", "/delete-author/**", "/delete-category/**",
                                 "/delete-publisher/**", "/books", "/add-book/**", "/delete-book/**", "/loans",
@@ -65,7 +71,9 @@ public class SpringSecurity {
                         .accessDeniedHandler((request, response, accessDeniedException) -> {
                             response.sendRedirect("/access_denied");
                         })
-                )                .build();
+                )
+                .headers(headers -> headers.disable())
+                .build();
     }
 
     @Autowired
